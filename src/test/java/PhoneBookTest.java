6c57193ed82aa80c1a0ec555e6b571509d156a01
Karine Simonian <ks_05@mail.ru>
2022-12-21 23:37:09 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    PhoneBook sut;
+
+    @BeforeEach
+    public void initEachTest() {
+        sut = new PhoneBook();
+    }
+
+    @Test
+    public void addTest() {
+        sut.add("Vasilii", "1111111");
+        sut.add("Oleg", "2222222");
+        sut.add("Oleg", "3333333");
+        int actual = sut.add("Irina", "4444444");
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
 }
